c0dbc3120ac8411ed7c535667df0ae3682063020
ForgotAlready <oleg.burdakin2016@yandex.ru>
2023-08-03 20:07:02 +0700

    On branch master  Initial commit  Changes to be committed: 	new file:   Assets/Assets.meta 	new file:   Assets/Assets/Field.png 	new file:   Assets/Assets/Field.png.meta 	new file:   Assets/Assets/Tic-Tac-Toe assets.png 	new file:   Assets/Assets/Tic-Tac-Toe assets.png.meta 	new file:   Assets/Editor.meta 	new file:   Assets/Editor/com.unity.mobile.notifications.meta 	new file:   Assets/Editor/com.unity.mobile.notifications/NotificationSettings.asset 	new file:   Assets/Editor/com.unity.mobile.notifications/NotificationSettings.asset.meta 	new file:   Assets/Prefabs.meta 	new file:   Assets/Prefabs/Cross.prefab 	new file:   Assets/Prefabs/Cross.prefab.meta 	new file:   Assets/Prefabs/Null.prefab 	new file:   Assets/Prefabs/Null.prefab.meta 	new file:   Assets/Scenes.meta 	new file:   Assets/Scenes/SampleScene.unity 	new file:   Assets/Scenes/SampleScene.unity.meta 	new file:   Assets/Scripts.meta 	new file:   Assets/Scripts/ClickReciver.cs 	new file:   Assets/Scripts/ClickReciver.cs.meta 	new file:   Assets/Scripts/MakeMoveScript.cs 	new file:   Assets/Scripts/MakeMoveScript.cs.meta 	new file:   Assets/Scripts/Pause.cs 	new file:   Assets/Scripts/Pause.cs.meta

---
+++ Assets/Scripts/ClickReciver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickReciver : MonoBehaviour, IPointerClickHandler
+{
+    public Cell clickedObject = null;
+    public static ClickReciver obj = null;
+
+    public void OnPointerClick(PointerEventData eventData){
+        GameObject pressed = eventData.pointerPress;
+        int n = MakeMoveScript.instance.field.size;
+
+        bool stopFlag = false;
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                if(MakeMoveScript.instance.field[i, j].getObject().name == pressed.name){
+                    if(MakeMoveScript.instance.field[i, j].getState() == State.EMPTY){
+                        MakeMoveScript.instance.field[i, j] = MakeMoveScript.instance.field[i, j].changeState(MakeMoveScript.instance.field[i, j], MakeMoveScript.instance.gameState);
+                        clickedObject = MakeMoveScript.instance.field[i, j];
+                        obj = this;
+                    }
+                    stopFlag = true;
+                    break;
+                }
+                if(stopFlag) break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MakeMoveScript.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public enum State{
+    CROSS,
+    NULL,
+    EMPTY
+}
+
+public abstract class ICell{
+    protected State state = State.EMPTY;
+    protected int number;
+}
+
+//Фабричный метод, честно хз зачем он, просто повыпендриваться, что я типо паттерны знаю
+public abstract class Creator{
+    public abstract ICell create(GameObject obj);
+}
+
+public class CellCreator : Creator{
+    public override ICell create(GameObject obj){return new Cell(obj);}
+}
+
+public class Cell : ICell{
+    private GameObject gameObject; // gameObject wich associate with cell
+
+    public Cell(GameObject obj){
+        gameObject = obj;
+    }
+
+    public GameObject getObject(){
+        return gameObject;
+    }
+
+    public State getState(){
+        return state;
+    }
+
+    private Cell setState(State state_){
+        state = state_;
+        return this;
+    }
+
+    public Cell changeState(Cell cell, State state){
+        if(cell.getState() != State.EMPTY) return cell;
+        else return cell.setState(state);
+    }
+
+    public void setNumber(int number_){
+        number = number_; // пока предпологаю, что ячейки можно перенумеровать.
+    }
+}
+
+public class Field{
+    private Cell[,] cells;
+    private int size_ = 3; // 3x3 matrix
+    private int counterRow = 0;
+    private int counterCol = 0;
+
+    public int size{
+        get{
+            return size_;
+        }
+        set{}
+    }
+
+    public Field(){
+        cells = new Cell[size_ , size_];
+    }
+
+    public void setCell(Cell cell){
+        if(counterRow == size_){
+            counterRow = 0;
+            counterCol++;
+        }
+        cells[counterCol, counterRow] = cell;
+        cell.setNumber(counterRow);
+        counterRow++;
+    }
+
+    public Cell this[int i, int j]{
+        get{return cells[i, j];}
+        set{cells[i, j] = value;}
+    }
+}
+
+public class WinChecker{
+    public static bool checkWin(Field field){
+        int n = field.size;
+        //Проверяем строки
+        int counter = 0;
+        for(int row = 0;row < n;row++){
+            counter = 0;
+            for(int i = 0;i < n;i++){
+                if(field[row, i].getState() == State.CROSS) counter++;
+                if(field[row, i].getState() == State.NULL) counter--;
+                if(field[row, i].getState() == State.EMPTY) break;
+            }
+            if(Math.Abs(counter) == n) return true;
+        }
+
+        //Проверяем столбцы
+        counter = 0;
+        for(int col = 0;col < n;col++){
+            counter = 0;
+            for(int i = 0;i < n;i++){
+                if(field[i, col].getState() == State.CROSS) counter++;
+                if(field[i, col].getState() == State.NULL) counter--;
+                if(field[i, col].getState() == State.EMPTY) break;
+            }
+            if(Math.Abs(counter) == n) return true;
+        }
+
+        //Проверяем главную диагональ
+        counter = 0;
+        for(int i = 0;i < n;i++){
+            if(field[i, i].getState() == State.CROSS) counter++;
+            if(field[i, i].getState() == State.NULL) counter--;
+            if(field[i, i].getState() == State.EMPTY) break;
+        }
+        if(Math.Abs(counter) == n) return true;
+
+        //Проверяем побочную диагональ
+        counter = 0;
+        for(int i = n - 1, j = 0;i >= 0;i--, j++){
+            if(field[j, i].getState() == State.CROSS) counter++;
+            if(field[j, i].getState() == State.NULL) counter--;
+            if(field[j, i].getState() == State.EMPTY) break;
+        }
+        if(Math.Abs(counter) == n) return true;
+
+        return false;
+    }
+}
+
+public class MakeMoveScript : MonoBehaviour
+{
+    [SerializeField] GameObject CrossPrefab;
+    [SerializeField] GameObject NullPrefab;
+    public Field field = new Field();
+    public CellCreator cellCreator = new CellCreator();
+    public GameObject[] cells;
+    public State gameState = State.CROSS;
+    public static MakeMoveScript instance;
+
+    public void changeGameState(){
+        if(gameState == State.CROSS) gameState = State.NULL;
+        else gameState = State.CROSS;
+    }
+    
+    void Start()
+    {
+        instance = this;
+        cells = GameObject.FindGameObjectsWithTag("Cell");
+        foreach (GameObject item in cells)
+        {
+            Cell temp = (Cell) cellCreator.create(item);
+            field.setCell(temp);
+        }
+    }
+    //Function that called every frame
+    void Update(){
+        // Если случилось нажатие, то нужно создать в нужной клетке крестик/нолик, наверно плохая идея делать это каждый фрейм, лучше лиснер повещать, но я так не умею
+        if(ClickReciver.obj != null)
+        {
+            Cell moveCell = ClickReciver.obj.clickedObject;
+            Transform instantiateCoords = moveCell.getObject().transform;
+
+            GameObject move = null;
+            //Вообще считается, что создавать объект - это крайне плохая практика, но я спорт прогер, мне пофиг
+            if(moveCell.getState() == State.CROSS){
+                move = Instantiate(CrossPrefab, instantiateCoords.position, Quaternion.identity);
+            }else{
+                move = Instantiate(NullPrefab, instantiateCoords.position, Quaternion.identity);
+            }
+
+            move.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform);
+            MakeMoveScript.instance.changeGameState();
+            ClickReciver.obj = null;
+            Pause.point.isOver = WinChecker.checkWin(field);
+        }
+    }
+}
